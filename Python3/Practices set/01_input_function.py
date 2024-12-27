43bd1b9a2566199209698516e1eaaaec3729ef07
Abhishek <87256096+abhishek-maurya576@users.noreply.github.com>
2024-12-27 20:21:36 +0530

    Add files via upload

---
+++ Python3/Practices set/01_input_function.py
@@ -0,0 +1,8 @@
+# Write a python program to display a user entered name followed by Good 
+# Afternoon using input () function
+
+name = input("Enter your name: ")
+print(name,"Good Maorning")
+print("Good Maorning",name)
+
+print(f"Good morning, {name}")  # Using f-string formatting
---
+++ Python3/Practices set/03_Detected_Double_Quote.py
@@ -0,0 +1,4 @@
+# Write a program to detect double space in a string
+text = "Hello  this is good boy"
+print(text.find("  ")) #5 index pr double space detected
+print(text.find("goo"))
