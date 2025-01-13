8e100327f07bc270e68b88718df9a8defedd629a
Abhishek <87256096+abhishek-maurya576@users.noreply.github.com>
2025-01-13 11:42:55 +0530

    Update 02_var.py

--- Python2/01Variable/02_var.py
+++ Python2/01Variable/02_var.py
@@ -3,3 +3,9 @@ y = 'alex'
 
 print(x)
 print(y)
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+z = 5 # z is of type int
+z = 'Variable' # z is now of type str
+
+print(z)
