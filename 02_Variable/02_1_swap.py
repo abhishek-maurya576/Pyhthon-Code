88c6f3ba71c16fab0a37f9b02a2f50cc523bab3b
abhisek-maurya576 <maurya972137@gmail.com>
2025-04-22 13:16:21 +0530

    create swap number

---
+++ 02_Variable/02_1_swap.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ennter first num: ")) #5
+num2 = int(input("Ennter second num: ")) #7
+num1 =num1+num2
+num2 = num1 - num2
+num1 = num1 - num2
+print("Var1 value: ",num1)
+print("var2 value: ",num2)
