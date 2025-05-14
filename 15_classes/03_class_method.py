eda1b5fdecb7ccbe1c61db8908768657bfd380b7
abhisek-maurya576 <maurya972137@gmail.com>
2025-05-14 13:09:17 +0530

    created new file

---
+++ 15_classes/03_class_method.py
@@ -0,0 +1,7 @@
+# Create a class called 'Calculator' with a method add(a, b) 
+# that returns the sum of two numbers.
+class Calculator:
+	def add(self,num1,num2):
+		return num1+num2
+obj = Calculator()
+print(obj.add(5,6))
---
+++ 15_classes/04_constructor_in_class.py
@@ -0,0 +1,9 @@
+# Create a class 'Car' with attributes brand and price.
+# Initialize them using the constructor (__init__) and print the details.
+class Car:
+    def __init__(self,brand,price):
+        self.brand = brand
+        self.price = price
+obj = Car('Toyota',500000)
+obj2 = Car('Honda',700000)
+obj.Car()
